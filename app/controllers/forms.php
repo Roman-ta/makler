0eb45c28d460d2b83290d98de2794b84015daca5
Roman-ta <romanta1680@gmail.com>
2025-01-31 16:10:48 +0200

    commit 3

--- app/controllers/forms.php
+++ app/controllers/forms.php
@@ -6,8 +6,15 @@ $db = Db::getInstance();
 
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-    if(isset($_POST['register'])){
-        $register = new Register($_POST);
-        $register->validateEmail();
+    if(isset($_POST['action']) && !empty($_POST['action'])){
+        if($_POST['action'] == 'register'){
+            require VIEWS . '/tpl/register-modal.html';
+            $register = new Register($_POST);
+            $register->validateEmail();
+        }
+        if($_POST['action'] == 'auth'){
+            require VIEWS . '/tpl/auth-modal.html';
+        }
     }
+
 }
--- public/script/register.js
+++ public/script/register.js
@@ -1,9 +1,14 @@
 $(document).ready(function () {
     $('.header__register a').click(function (e) {
         e.preventDefault()
+        if($('.auth').find('.modal-register').length > 0){
+            $('.modal-register').css('display', 'block');
+            return;
+        }
         $.ajax({
-            url: "register",
+            url: "forms",
             method: "POST",
+            data : {action : 'register'},
             success: function (response) {
                 $('.auth').append(response)
 
@@ -14,15 +19,14 @@ $(document).ready(function () {
     })
 
     $('.auth').on('click', '.modal-block__close', function () {
-        $('.auth__container').css('display', 'none');
+        $('.modal-register').css('display', 'none');
     })
 
-// Ползунок
-    $('.slider-method__line').click(function (){
+// Ползунок переключения Phone - email
+    $('.auth').on('click', '.slider-method__line', function () {
         $('.slider-method__circle').toggleClass('slider-method__active');
         $('.modal-form__email').toggleClass('modal-form__active');
         $('.modal-form__phone').toggleClass('modal-form__disable');
     })
-
 });
 
